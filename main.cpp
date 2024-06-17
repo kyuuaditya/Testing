53ea71cb4819304adb58f786c57b8d74901dac73
Aditya <thisisaditya.me@gmail.com>
2024-06-18 01:01:46 +0530

    testing rand()

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+#include <stdio.h>
+using namespace std;
+
+int main() {
+    // to check the randomness of rand() and use-case of srand(time(0))
+    /*
+    srand(time(0));
+    int iterations = 100000;
+    int count[10] = {0};
+    int temp;
+    for (int i = 0; i < iterations; i++) {
+        temp = rand() % 10;
+        count[temp]++;
+    }
+    for (int i = 0; i < 10; i++) {
+        cout << count[i] << " ";
+    }
+    */
+}
