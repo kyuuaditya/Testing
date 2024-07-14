001df779c9e79395c43ba389b1b0530d5babc307
Aditya <thisisaditya.me@gmail.com>
2024-07-13 20:42:32 -0700

    testing fstream

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
-#include <bits/stdc++.h>
-#include <stdio.h>
+#include<iostream>
+#include<fstream>
 using namespace std;
+
+#define MAX_LINES 1000
 
 int main() {
     // to check the randomness of rand() and use-case of srand(time(0))
@@ -17,4 +19,38 @@ int main() {
         cout << count[i] << " ";
     }
     */
+
+   string filename;
+   ifstream file;
+   string array[MAX_LINES];
+
+   cout<<"File name: ";
+   cin>>filename;
+
+   file.open(filename);
+
+   if(file.fail()){
+    cout<<"File failed to oprn."<<endl;
+    return 1;
+   }
+
+   int lines=0;
+
+   while(!file.eof()){
+    getline(file,array[lines]);
+    lines++;
+
+    if(lines==MAX_LINES){
+        cout<<"Max storage reached"<<endl;
+        break;
+    }
+   }
+
+   file.close();
+
+   for(int i=0;i<lines;i++){
+    cout<<array[i]<<endl;
+   }
+
+   return 0;
 }
