8fa5eb5d8a2f39c1fea16cbb83e326194d7cddc5
Aditya <thisisaditya.me@gmail.com>
2024-07-15 00:52:45 -0700

    testing files

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 #include<iostream>
 #include<fstream>
+#include "CImg.h"
 using namespace std;
+using namespace cimg_library;
 
-#define MAX_LINES 1000
+
+// #define MAX_LINES 1000
 
 int main() {
     // to check the randomness of rand() and use-case of srand(time(0))
@@ -20,6 +23,7 @@ int main() {
     }
     */
 
+   /*
    string filename;
    ifstream file;
    string array[MAX_LINES];
@@ -52,5 +56,28 @@ int main() {
     cout<<array[i]<<endl;
    }
 
-   return 0;
+   */
+  // CImg<unsigned char> img("lena.jpeg"),visu(758,438,1,3);
+
+  // CImg<unsigned char> img(758,438,1,3);
+  // img.fill(0);
+  // img.noise(128);
+  // img.blur(2,0,0);
+
+  CImg<unsigned char> image("lena.jpeg");
+  CImg<unsigned char> vis(500,400,1,3,0);
+const unsigned char red[] = { 255,0,0 }, green[] = { 0,255,0 }, blue[] = { 0,0,255 };
+image.blur(2.5);
+CImgDisplay main_disp(image,"Click a point"), draw_disp(vis,"Intensity profile");
+while (!main_disp.is_closed() && !draw_disp.is_closed()) {
+main_disp.wait();
+if (main_disp.button() && main_disp.mouse_y()>=0) {
+const int y = main_disp.mouse_y();
+vis.fill(0).draw_graph(image.get_crop(0,y,0,0,image.width()-1,y,0,0),red,1,1,0,255,0);
+vis.draw_graph(image.get_crop(0,y,0,1,image.width()-1,y,0,1),green,1,1,0,255,0);
+vis.draw_graph(image.get_crop(0,y,0,2,image.width()-1,y,0,2),blue,1,1,0,255,0);
+vis.display(draw_disp);
+}
+}
+return 0;
 }
